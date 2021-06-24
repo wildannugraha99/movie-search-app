d5e45a31516d3de009cdad0c93c0826e1b8e2674
wildannugraha99 <mwnwildan99@gmail.com>
2021-06-24 22:10:21 +0700

    update v2

--- script.js
+++ script.js
@@ -1,77 +1,156 @@
-function runningData() { 
+function runningData() {
     $('.list-film').html('');
     $('.labels').html('');
 
     const inputSearch = document.querySelector('#input-search');
-    
-    const labelResult=document.createElement('h2');
-    labelResult.innerText = "Results for " + inputSearch.value+':';
+
+    const labelResult = document.createElement('h2');
+    labelResult.innerText = "Results for " + inputSearch.value + ':';
 
     html = document.querySelector('.labels');
     html.append(labelResult);
 
 
-    
+
     $.ajax({
         type: "GET",
-        url:'https://www.omdbapi.com/?apikey=49e1cf87&s='+inputSearch.value,
+        url: 'https://www.omdbapi.com/?apikey=49e1cf87&s=' + inputSearch.value,
         dataType: "json",
         success: function (data) {
-            
-           if(data.Response == 'True'){
-           
-            $.map(data,function(i){
-                
-                for(i=0; i <=10; i++){
-                    const image = document.createElement('div');
-                    image.innerHTML=`<img src="${data.Search[i].Poster}" alt="Movie image">`;
+            console.log(data)
+            if (data.Response == 'True') {
 
-                    const titleMovie = document.createElement("h3");
-                    titleMovie.innerText = data.Search[i].Title;
-                    
-                    const typeMovie= document.createElement('p')
-                    typeMovie.innerText = 'Type: '+ data.Search[i].Type;
-                   
-                    const year = document.createElement('p')
-                    year.innerText = 'Year: ' + data.Search[i].Year;
-                    
-                    const inner = document.createElement("div");
-                    inner.classList.add("inner")
-                    inner.append(image,titleMovie,typeMovie,year);
-                    
-                    const cards = document.createElement('div');
-                    cards.classList.add('cards');
-                    cards.append(inner);
-                
-                    
-                    container = document.querySelector('.list-film');
-                    container.append(cards);
-                }
-            })
-           
+                $.map(data, function (i) {
+
+                    for (i = 0; i <= 10; i++) {
+                        const image = document.createElement('div');
+                        image.innerHTML = `<img src="${data.Search[i].Poster}" alt="Movie image">`;
+
+                        const titleMovie = document.createElement("h3");
+                        titleMovie.innerText = data.Search[i].Title;
+
+                        const typeMovie = document.createElement('p')
+                        typeMovie.innerText = 'Type: ' + data.Search[i].Type;
+
+                        const year = document.createElement('p')
+                        year.innerText = 'Year: ' + data.Search[i].Year;
+
+                        const buttonDetail = document.createElement('div')
+                        buttonDetail.classList.add('btnDetail');
+                        buttonDetail.innerHTML =`<button data-id="${data.Search[i].imdbID}" class="btnDetails"
+                        data-bs-toggle="modal" data-bs-target="#exampleModal">Detail <span><i class="fas fa-info-circle"></i></span></button>`
+
+                        const inner = document.createElement("div");
+                        inner.classList.add("inner")
+                        inner.append(image, titleMovie, typeMovie, year, buttonDetail);
+
+                        const cards = document.createElement('div');
+                        cards.classList.add('cards');
+                        cards.append(inner);
 
 
-           }else{
-            const notFound = document.createElement('h1');
-            notFound.classList.add('notFound')
-            notFound.innerText = 'Oops... movie not found!'
-            container = document.querySelector('.list-film');
-            container.append(notFound);
-           }
+                        container = document.querySelector('.list-film');
+                        container.append(cards);
+
+
+
+
+                    }
+
+                })
+
+
+
+
+            } else {
+                const notFound = document.createElement('h1');
+                notFound.classList.add('notFound')
+                notFound.innerText = 'Oops... movie not found!'
+                container = document.querySelector('.list-film');
+                container.append(notFound);
+            }
         }
     });
- }
+}
+
+function deleteData() {
+    const inputSearch = document.querySelector('#input-search');
+
+    $.ajax({
+        type: "GET",
+        url: 'https://www.omdbapi.com/?apikey=49e1cf87&s=' + inputSearch.value,
+        dataType: "json",
+        success: function () {
+            $('.list-film').html('');
+            $('.labels').html('');
+            $('#input-search').val('');
+
+        }
+    });
+}
+
+
 
 
 
 const searchButton = document.querySelector('.input-button');
-searchButton.addEventListener('click', function (){
-   runningData();
+searchButton.addEventListener('click', function () {
+    runningData();
 })
-    
+
 window.addEventListener('keydown', (event) => {
     if (event.code === 'Enter') {
         runningData();
     }
-  });
+});
 
+const deleteButton = document.querySelector('.input-button-delete');
+deleteButton.addEventListener('click', function () {
+    deleteData();
+})
+
+$(".list-film").on('click', ".btnDetails", function () {
+        console.log($(this).data('id'))
+        $.ajax({
+            type: "GET",
+            url: 'https://www.omdbapi.com/?apikey=49e1cf87&i=' + $(this).data('id'),
+            dataType: "json",
+            success: function (film) {
+                if (film.Response == 'True') {
+
+                    $('.modal-body').html(`
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-4">
+                            <img src="${film.Poster}" alt="Movie image" class="img-fluid">
+                            </div>
+                            <div class="col-md-8">
+                                <ul>
+                                    <li class="no-list-1"><h4>${film.Title}</h4></li>
+                                    <li>Genre: ${film.Genre}</li>
+                                    <li>Rated: ${film.Rated}</li>
+                                    <li>Released: ${film.Released}</li>
+                                    <li>Director: ${film.Director}</li>
+                                    <li>Writer: ${film.Writer}</li>
+                                    <li>Plot:</li>
+                                    <li class="no-list-2">${film.Plot}</li>
+                                    
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                `)
+                    $('.modal-footer').html(`
+                        <div class="container-fluid">
+                            <h5><span><i class="fas fa-star"></i> </span> IMDB Rating: ${film.imdbRating}</h5>
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
+            `)
+                } else {
+
+                }
+            }
+        });
+    })
+
+    
